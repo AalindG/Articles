b4e09eded56908259d8a1691081b1705c504c464
Aalind Gupta <aalindg106@gmail.com>
2017-11-22 14:17:32 +0530

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.delete-article').on('click', function(e){
+		$target = $(e.target);
+		const id = $target.attr('data-id');
+		console.log('id: ', id)
+
+		$.ajax({
+			type: 'DELETE',
+			url: '/articles/'+id,
+			success: (response)=>{
+				alert('Deleting Article');
+				window.location.href="/"
+			},
+			error: (err)=>{
+				console.log('Error while deleting: ', err);
+			}
+		})
+	});
+
+	
+
+})
+
